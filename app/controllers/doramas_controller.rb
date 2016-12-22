98420566d92004e5b5f6eec396ed1514fd974358
KUBOKAZUYA <jpkk623@gmail.com>
2016-12-22 13:01:40 +0900

    コメントで説明した

--- app/controllers/doramas_controller.rb
+++ app/controllers/doramas_controller.rb
@@ -9,19 +9,34 @@ class DoramasController < ApplicationController
 
   def new
     @dorama = Dorama.new
-    @dorama = Dorama.find_by(title: params[:title], story: params[:stroy])
   end
 
   def create
-    @dorama = Dorama.new(title: params[:title], story: params[:stroy])
-    if @dorama.save!
+
+    #
+    # [:dorama]ってのが抜けてたからだめやった。paramsの中のdoramaの中にtitleとかstoryとかがまとまって入ってたねん。
+    #
+    @dorama = Dorama.new(title: params[:title], story: params[:story]) # ←以前の書き方
+    @dorama = Dorama.new(title: params[:dorama][:title], story: params[:dorama][:story]) # ←正解の書き方
+
+    #
+    # 正解の書き方で通るんやけど、さらに安全に書くには以下の書き方がいい。理由はしたのdorama_paramsに書いてる。
+    #
+    @dorama = Dorama.new(dorama_params)
+
+
+    if @dorama.save
       redirect_to @dorama, notice: "登録されました。"
     else
       render 'new'
     end
   end
 
+  #
+  # ↓こいつはユーザーのリクエストしてきたパラメーターをまとめてるもの。と同時に悪意のあるパラメータをなげられないようにpermitでリクエストできるものを制限してる。
+  #
+  private
   def dorama_params
-    params.require(:dorama).permit(:story, :title)
+    params.require(:dorama).permit(:story, :title, :image)
   end
 end
