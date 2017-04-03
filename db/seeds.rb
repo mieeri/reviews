c12d500d66043be232e907bf339ebc92410f9788
三枝江梨香 <zyny0215@gmail.com>
2017-04-03 16:45:28 +0900

    herokuにアップ

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'zyny0215@gmail.com', password: ENV['ADMIN_USER_PASSWORD'], password_confirmation: ENV['ADMIN_USER_PASSWORD'])
 
 
