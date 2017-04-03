fb3aef3f87f167e61689495db74566b16809c07f
三枝江梨香 <zyny0215@gmail.com>
2017-04-03 17:01:31 +0900

    adminuserのアドレス際変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-AdminUser.create!(email: 'hop.step.jumpinpin@gmail.com', password: ENV['ADMIN_USER_PASSWORD'], password_confirmation: ENV['ADMIN_USER_PASSWORD'])
+AdminUser.create!(email: 'zyny0215@gmail.com', password: ENV['ADMIN_USER_PASSWORD'], password_confirmation: ENV['ADMIN_USER_PASSWORD'])
 
 
