a2e6077a7fa2d8666c722d2d10943a44be7d8072
三枝江梨香 <zyny0215@gmail.com>
2017-04-21 11:32:24 +0900

    dorama更新

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
   def index
-    @tweets = TwitterClient.get_client.search("#" + "#{@dorama.hash_tag}", lang: "ja", result_type: "recent", since_id: 0, exclude: "retweets").take(20)
+    @tweets = TwitterClient.get_client.search("#小さな巨人", lang: "ja", result_type: "recent", since_id: 0, exclude: "retweets").take(20)
   end
 end
